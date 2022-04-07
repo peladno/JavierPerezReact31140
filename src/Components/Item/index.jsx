9ac2ff6c8fd0e3bfc77d455fdea1f7304d79f8b0
j.perezu <j.perezurrutia@gmail.com>
2022-04-07 18:15:48 -0400

    se cambiaron un par de cosas por que lña tarjeta se extendia en toda la pagina

--- src/Components/Item/index.jsx
+++ src/Components/Item/index.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
 
 
 export default function Item({id, name, price, description}) {
   return (
     <>
-    <Card key={id} sx={{ minWidth: 275 }}>
+    <Card key={id} sx={{ maxWidth: 275 }}>
       <CardContent>
         <Typography variant="h5" component="div">
           {name}
@@ -19,6 +21,9 @@ export default function Item({id, name, price, description}) {
           ${price}
         </Typography>
       </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
     </Card>
     </>
   );
